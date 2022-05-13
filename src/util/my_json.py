e9990983bf3ececfc7f1252dee808dc70025735c
wazam <wazam123@gmail.com>
2022-05-13 02:10:54 -0400

    refactor: add json util

---
+++ src/util/my_json.py
@@ -0,0 +1,13 @@
+import json
+
+# function to add to JSON file
+def write_json(new_data, filename='data.json'):
+    with open(filename,'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside players
+        file_data["players"].append(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent = 4)
