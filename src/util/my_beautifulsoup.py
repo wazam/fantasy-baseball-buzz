bd2121ed2a3f1be5df0fbcb5df0c750bc27c17f0
wazam <wazam123@gmail.com>
2022-05-14 05:29:25 -0400

    chore: add to beautifulsoup util

--- src/util/my_beautifulsoup.py
+++ src/util/my_beautifulsoup.py
@@ -1,28 +1,37 @@
-import requests
 from bs4 import BeautifulSoup
 
+import util.my_ratelimit as MyR
 
-# Scrape web_page for class_
-def scrape_class(url, headers, all, name, attrib, href):
-    page = requests.get(url, headers= headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    results = find_class(soup, all, name, attrib, href)
+
+# Return class tag(s) from web page
+def scrape_class(url_import, headers_import, find_all, find_name, find_attrib, href_import):
+    response = MyR.make_ratelimited_request(url_import, headers_import)
+    soup = BeautifulSoup(markup = response.content, features = 'html.parser')
+    results = find_class(soup, find_all, find_name, find_attrib, href_import)
     return results
 
 
-# Find in results_page or html_soup for class_
-def find_class(soup, all, name, attrib, href):
-    if all == 'all':
-        results = soup.find_all(name, class_= attrib, href= href)
+# Return class tag(s) from soup
+def find_class(soup_import, find_all, find_name, find_attrib, href_import):
+    if find_all == 'all':
+        results = soup_import.find_all(find_name, class_ = find_attrib, href = href_import)
     else:
-        results = soup.find(name, class_= attrib, href= href)
+        results = soup_import.find(find_name, class_ = find_attrib, href = href_import)
     return results
 
 
-# Find in results_page or html_soup for class_
-def find(soup, all, name, href):
-    if all == 'all':
-        results = soup.find_all(name, href= href)
+# Return search result(s) from web page
+def scrape(url_import, headers_import, find_all, find_name, href_import):
+    response = MyR.make_ratelimited_request(url_import, headers_import)
+    soup = BeautifulSoup(markup = response.content, features = 'html.parser')
+    results = find(soup, find_all, find_name, href_import)
+    return results
+
+
+# Return search result(s) from soup
+def find(soup_import, find_all, find_name, href_import):
+    if find_all == 'all':
+        results = soup_import.find_all(find_name, href = href_import)
     else:
-        results = soup.find(name, href= href)
+        results = soup_import.find(find_name, href = href_import)
     return results
