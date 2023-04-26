cded2e3f50fb45aec2b30923b8b2c52d8c391a35
wazam <wazam123@gmail.com>
2023-04-26 00:52:58 -0400

    add testing

--- src/utils/my_unidecode.py
+++ src/utils/my_unidecode.py
@@ -1,7 +1,13 @@
-import unidecode
+from unidecode import unidecode
 
 
 # Returns ASCII characters from unicode text data
 def fix_str(unicode_string):
-    ASCII_string = unidecode.unidecode(unicode_string)
+    ASCII_string = unidecode(unicode_string)
     return ASCII_string
+
+
+# Used for testing with `pipenv run python src/utils/my_unidecode.py` or `pipenv run python -m src.utils.my_unidecode`
+if __name__ == "__main__":
+    data = fix_str("Ronald Jose Acuña Jr")
+    print(data)
