d7db32999ce94f5ce18bf50cf4c0028bf547a73b
wazam <28695573+wazam@users.noreply.github.com>
2023-05-09 02:22:50 -0400

    add new util datetime func

--- src/util_datetime.py
+++ src/util_datetime.py
@@ -1,3 +1,4 @@
+import time
 from datetime import date
 from datetime import timedelta
 
@@ -8,6 +9,13 @@ def date_X_days_ago(days):
     date_days_back = timedelta(days=days)
     date_back = date_today - date_days_back
     return date_back
+
+
+# Returns time in HH:MM:SS format for use in log terminal with print()
+def get_time_for_logs():
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    return current_time
 
 
 # Tests with `pipenv run python src/util_datetime.py`
